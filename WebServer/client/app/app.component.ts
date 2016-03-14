db7ea5d1c8f771f5ad525762c0247aba05109295
avivh <shfanfan@yahoo.com>
2016-03-14 20:00:27 +0200

    some enhancements while learning angular2...

--- WebServer/client/app/app.component.ts
+++ WebServer/client/app/app.component.ts
@@ -2,6 +2,24 @@ import {Component} from 'angular2/core';
 
 @Component({
 	selector: 'my-app',
-	template: '<h1>smarty main page</h1>'
+	template: `
+				<h1>smarty {{foo.name}} main page    {{cars|json}}  </h1>
+				<ul  [style.color]="color" >
+					<li *ngFor="#car of cars" > {{car}} 
+					</li>
+				</ul>
+				<button (click)="btnClick('click')" (blur)="log('blur')">press me :)</button>
+				<input [(ngModel)]="text" />
+				<h3>{{text}}</h3>
+`
+
 })
-export class AppComponent {}
+export class AppComponent {
+	cars = ['audi', 'bentli' ,'ford' ]
+	foo ={ name:'aviv'}
+	color="red";
+	text = "text here"
+	log = (msg){console.log(msg)}
+	btnClick =  (msg){this.text += "."}
+	
+}
